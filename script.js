b9554dc8694726be3b5266938a347bbebef8f00c
Demysel <84205147+Demysel@users.noreply.github.com>
2024-12-29 20:38:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAU19mm6RDWy_fhoMo3SLWFuyvT4UDKKKk",
+  authDomain: "calendrier-300cb.firebaseapp.com",
+  projectId: "calendrier-300cb",
+  storageBucket: "calendrier-300cb.firebasestorage.app",
+  messagingSenderId: "556904721649",
+  appId: "1:556904721649:web:8d40a30990520e4ffbcd50",
+  measurementId: "G-9G8217QW9D"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 // Initialisation Firebase
 const db = getDatabase();
 
